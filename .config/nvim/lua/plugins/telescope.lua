a38f574ab3e58646bf9b7319a92b92a0420dd949
keonly <keon.lee@proton.me>
2024-01-04 21:37:02 +0900

    feat: update nvim config

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,14 +1,45 @@
-local keymap = vim.keymap
+local mapkey = require("util.keymapper").mapkey
+
+local config = function()
+	local telescope = require("telescope")
+	telescope.setup({
+		defaults = {
+			mappings = {
+				i = {
+					["<C-j>"] = "move_selection_next",
+					["<C-k>"] = "move_selection_previous",
+				},
+			},
+		},
+		pickers = {
+			find_files = {
+				theme = "dropdown",
+				previewer = false,
+				hidden = true,
+			},
+			live_grep = {
+				theme = "dropdown",
+				previewer = false,
+			},
+			buffers = {
+				theme = "dropdown",
+				previewer = false,
+			},
+		},
+	})
+end
 
 return {
-    "nvim-telescope/telescope.nvim",
-    tag = "0.1.5",
-    lazy = false,
-    dependencies = { "nvim-lua/plenary.nvim" },
-    keys = {
-        keymap.set("n", "<leader>ff", ":Telescope find_files<CR>"),
-        keymap.set("n", "<leader>fa", ":Telescope<CR>"),
-        keymap.set("n", "<leader>fg", ":Telescope live_grep<CR>"),
-        keymap.set("n", "<leader>fb", ":Telescope buffers<CR>"),
-    },
+	"nvim-telescope/telescope.nvim",
+	tag = "0.1.3",
+	lazy = false,
+	dependencies = { "nvim-lua/plenary.nvim" },
+	config = config,
+	keys = {
+		mapkey("<leader>fk", "Telescope keymaps", "n"),
+		mapkey("<leader>fh", "Telescope help_tags", "n"),
+		mapkey("<leader>ff", "Telescope find_files", "n"),
+		mapkey("<leader>fg", "Telescope live_grep", "n"),
+		mapkey("<leader>fb", "Telescope buffers", "n"),
+	},
 }
