5cfe184ebd7d8653b22c9453498ab35e32247511
keonly <keon.lee@proton.me>
2024-04-26 14:15:20 +0900

    feat: add nvim-dap, nvim-dap-ui

--- .config/nvim/lua/plugins/nvim-dap.lua
+++ .config/nvim/lua/plugins/nvim-dap.lua
@@ -1,28 +1,38 @@
 local config = function()
-	local dap = require("dap")
-	local dapui = require("dapui")
+    local dap = require("dap")
+    local dapui = require("dapui")
 
-	dap.listeners.before.attach.dapui_config = function()
-		dapui.open()
-	end
-	dap.listeners.before.launch.dapui_config = function()
-		dapui.open()
-	end
-	dap.listeners.before.event_terminated.dapui_config = function()
-		dapui.close()
-	end
-	dap.listeners.before.event_exited.dapui_config = function()
-		dapui.close()
-	end
-
-	vim.keymap.set("n", "<leader>db", dap.toggle_breakpoint, {})
-	vim.keymap.set("n", "<leader>dc", dap.continue, {})
+    dap.listeners.before.attach.dapui_config = function()
+        dapui.open()
+    end
+    dap.listeners.before.launch.dapui_config = function()
+        dapui.open()
+    end
+    dap.listeners.before.event_terminated.dapui_config = function()
+        dapui.close()
+    end
+    dap.listeners.before.event_exited.dapui_config = function()
+        dapui.close()
+    end
 end
 
 return {
-	"mfussenegger/nvim-dap",
-	dependencies = {
-		"rcarriga/nvim-dap-ui",
-	},
-	config = config,
+    "mfussenegger/nvim-dap",
+    dependencies = {
+        "nvim-neotest/nvim-nio",
+        "rcarriga/nvim-dap-ui",
+    },
+    config = config,
+    keys = {
+        {
+            "<leader>db",
+            function() require("dap").toggle_breakpoint() end,
+            { desc = "Toggle breakpoint" },
+        },
+        {
+            "<leader>dc",
+            function() require("dap").continue() end,
+            { desc = "Continue execution" },
+        },
+    },
 }
