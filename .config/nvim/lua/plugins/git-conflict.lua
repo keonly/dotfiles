554c15c74c754738ca5580ffddb9bf4c5c2d4d22
keonly <keon.lee@proton.me>
2024-04-26 14:31:47 +0900

    feat: add keybinds for git-conflict

--- .config/nvim/lua/plugins/git-conflict.lua
+++ .config/nvim/lua/plugins/git-conflict.lua
@@ -3,4 +3,13 @@ return {
     lazy = false,
     version = "*",
     config = true,
+    keys = {
+        { "<leader>gco", "<cmd>GitConflictChooseOurs<CR>",   desc = "Git Conflict: Choose ours" },
+        { "<leader>gct", "<cmd>GitConflictChooseTheirs<CR>", desc = "Git Conflict: Choose theirs" },
+        { "<leader>gca", "<cmd>GitConflictChooseBoth<CR>",   desc = "Git Conflict: Choose both" },
+        { "<leader>gcx", "<cmd>GitConflictChooseNone<CR>",   desc = "Git Conflict: Choose none" },
+        { "<leader>gcj", "<cmd>GitConflictNextConflict<CR>", desc = "Git Conflict: Next" },
+        { "<leader>gck", "<cmd>GitConflictPrevConflict<CR>", desc = "Git Conflict: Previous" },
+        { "<leader>gcl", "<cmd>GitConflictListQf<CR>",       desc = "Git Conflict: Show list" },
+    },
 }
