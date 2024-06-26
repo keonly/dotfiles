f5465501056b5fea3af79cdaa38b933a86bd0411
keonly <keon.lee@proton.me>
2024-06-26 13:59:02 +0900

    feat(nvim): enable mini.nvim plugins

---
+++ config/nvim/lua/plugins/mini.lua
@@ -0,0 +1,17 @@
+local config = function()
+	require("mini.ai").setup()
+	require("mini.bufremove").setup()
+	require("mini.comment").setup()
+	require("mini.cursorword").setup()
+	require("mini.jump").setup()
+	require("mini.operators").setup()
+	require("mini.pairs").setup()
+	require("mini.surround").setup()
+end
+
+return {
+	"echasnovski/mini.nvim",
+	version = false,
+	config = config,
+	lazy = false,
+}
