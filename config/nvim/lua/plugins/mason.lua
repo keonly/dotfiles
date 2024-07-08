b213c527c7589f799a21b4b4488f8b8ae82d56af
keonly <70371393+keonly@users.noreply.github.com>
2024-07-08 11:15:26 +0900

    chore(nvim): move mason-lspconfig to mason

--- config/nvim/lua/plugins/mason.lua
+++ config/nvim/lua/plugins/mason.lua
@@ -1,14 +1,29 @@
+local opts = {
+	ensure_installed = {
+		"efm",
+	},
+	automatic_installation = true,
+}
+
 return {
-    "williamboman/mason.nvim",
-    cmd = "Mason",
-    event = "BufReadPre",
-    config = {
-        ui = {
-            icons = {
-                package_installed = "✓",
-                package_pending = "➜",
-                package_uninstalled = "✗",
-            },
-        },
-    },
+	{
+		"williamboman/mason.nvim",
+		cmd = "Mason",
+		event = "BufReadPre",
+		config = {
+			ui = {
+				icons = {
+					package_installed = "✓",
+					package_pending = "➜",
+					package_uninstalled = "✗",
+				},
+			},
+		},
+	},
+
+	{
+		"williamboman/mason-lspconfig.nvim",
+		opts = opts,
+		event = "BufReadPre",
+	},
 }
