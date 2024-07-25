a2befd18b11f9cf51b8952fe0329757af2bedd2f
keonly <70371393+keonly@users.noreply.github.com>
2024-07-25 20:01:46 +0900

    fix(nvim): fold

--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -7,6 +7,7 @@ opt.softtabstop = 4
 opt.expandtab = true
 opt.smartindent = true
 opt.wrap = false
+opt.textwidth = 80
 
 -- Search
 opt.incsearch = true
@@ -15,14 +16,22 @@ opt.smartcase = true
 opt.hlsearch = false
 
 -- Appearance
+opt.numberwidth = 4
 opt.number = true
 opt.relativenumber = true
 opt.termguicolors = true
-opt.colorcolumn = "100"
+opt.colorcolumn = "80"
 opt.signcolumn = "yes"
 opt.cmdheight = 1
 opt.scrolloff = 10
 opt.completeopt = "menuone,noinsert,noselect"
+
+-- Fold
+opt.foldcolumn = "1"
+opt.foldlevel = 99
+opt.foldlevelstart = 99
+opt.foldenable = true
+opt.fillchars = [[eob: ,fold: ,foldopen:⌄,foldsep: ,foldclose:]]
 
 -- Behaviour
 opt.hidden = true
--- config/nvim/lua/plugins/statuscol.lua
+++ config/nvim/lua/plugins/statuscol.lua
@@ -3,30 +3,20 @@ return {
 	config = function()
 		local builtin = require("statuscol.builtin")
 		require("statuscol").setup({
-			relculright = true,
 			segments = {
-				{ text = { builtin.foldfunc }, click = "v:lua.ScFa" },
 				{
-					sign = { namespace = { "diagnostic/signs" }, maxwidth = 2, auto = true },
-					click = "v:lua.ScSa",
+					text = { builtin.foldfunc, " " },
+					click = "v:lua.ScFa",
+					hl = "Comment",
 				},
+				{ text = { "%s" }, click = "v:lua.ScSa" },
+				{ text = { builtin.lnumfunc, " " }, click = "v:lua.ScLa" },
 				{
-					text = function()
-						local rln = vim.fn.relnum()
-						return rln == 0 and " " or string.format("%2d ", rln)
-					end,
-					condition = { true },
-					style = "StatusColRelLine",
-				},
-				{
-					text = function()
-						return string.format("%3d", vim.fn.lnum())
-					end,
-					condition = { true },
-					style = "StatusColAbsLine",
-				},
-				{
-					sign = { name = { ".*" }, maxwidth = 2, colwidth = 1, auto = true, wrap = true },
+					sign = {
+						namespace = { "gitsign" },
+						maxwidth = 1,
+						auto = true,
+					},
 					click = "v:lua.ScSa",
 				},
 			},
