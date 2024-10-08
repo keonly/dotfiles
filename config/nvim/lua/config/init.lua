e9b361acc011240d5bde49d5af9482785e47daaa
keonly <70371393+keonly@users.noreply.github.com>
2024-10-08 11:55:45 +0900

    refactor(nvim): move lazy.nvim opts to lazy.lua

--- config/nvim/lua/config/init.lua
+++ config/nvim/lua/config/init.lua
@@ -2,20 +2,3 @@ require("config.globals")
 require("config.keymaps")
 require("config.lazy")
 require("config.options")
-
-local opts = {
-	defaults = {
-		lazy = true,
-	},
-	performance = {
-		rtp = {
-			disabled_plugins = {
-				"netrw",
-				"netrwPlugin",
-			},
-		},
-	},
-	change_detection = {
-		notify = true,
-	},
-}
