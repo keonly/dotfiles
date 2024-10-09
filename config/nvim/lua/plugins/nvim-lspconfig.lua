0cfabde55f0f931d41121ec55594d99ef7ad8f11
keonly <70371393+keonly@users.noreply.github.com>
2024-10-10 01:53:59 +0900

    chore(nvim): format

--- config/nvim/lua/plugins/nvim-lspconfig.lua
+++ config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,113 +1,112 @@
 local diagnostic_signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
 local on_attach = function(client, bufnr)
-    local opts = { noremap = true, silent = true, buffer = bufnr }
+	local opts = { noremap = true, silent = true, buffer = bufnr }
 
-    vim.keymap.set("n", "<leader>fd", "<cmd>Lspsaga finder<CR>", opts)               -- go to definition
-    vim.keymap.set("n", "<leader>gd", "<cmd>Lspsaga peek_definition<CR>", opts)      -- peek definition
-    vim.keymap.set("n", "<leader>gD", "<cmd>Lspsaga goto_definition<CR>", opts)      -- go to definition
-    vim.keymap.set("n", "<leader>ca", "<cmd>Lspsaga code_action<CR>", opts)          -- see available code actions
-    vim.keymap.set("n", "<leader>rn", "<cmd>Lspsaga rename<CR>", opts)               -- smart rename
-    vim.keymap.set("n", "<leader>nl", "<cmd>Lspsaga show_line_diagnostics<CR>", opts) -- show  diagnostics for line
-    vim.keymap.set("n", "<leader>nc", "<cmd>Lspsaga show_cursor_diagnostics<CR>", opts) -- show diagnostics for cursor
-    vim.keymap.set("n", "<leader>np", "<cmd>Lspsaga diagnostic_jump_prev<CR>", opts) -- jump to prev diagnostic in buffer
-    vim.keymap.set("n", "<leader>nn", "<cmd>Lspsaga diagnostic_jump_next<CR>", opts) -- jump to next diagnostic in buffer
-    vim.keymap.set("n", "K", "<cmd>Lspsaga hover_doc<CR>", opts)                     -- show documentation for what is under cursor
+	vim.keymap.set("n", "<leader>fd", "<cmd>Lspsaga finder<CR>", opts) -- go to definition
+	vim.keymap.set("n", "<leader>gd", "<cmd>Lspsaga peek_definition<CR>", opts) -- peek definition
+	vim.keymap.set("n", "<leader>gD", "<cmd>Lspsaga goto_definition<CR>", opts) -- go to definition
+	vim.keymap.set("n", "<leader>ca", "<cmd>Lspsaga code_action<CR>", opts) -- see available code actions
+	vim.keymap.set("n", "<leader>rn", "<cmd>Lspsaga rename<CR>", opts) -- smart rename
+	vim.keymap.set("n", "<leader>nl", "<cmd>Lspsaga show_line_diagnostics<CR>", opts) -- show  diagnostics for line
+	vim.keymap.set("n", "<leader>nc", "<cmd>Lspsaga show_cursor_diagnostics<CR>", opts) -- show diagnostics for cursor
+	vim.keymap.set("n", "<leader>np", "<cmd>Lspsaga diagnostic_jump_prev<CR>", opts) -- jump to prev diagnostic in buffer
+	vim.keymap.set("n", "<leader>nn", "<cmd>Lspsaga diagnostic_jump_next<CR>", opts) -- jump to next diagnostic in buffer
+	vim.keymap.set("n", "K", "<cmd>Lspsaga hover_doc<CR>", opts) -- show documentation for what is under cursor
 end
 
 return {
-    "neovim/nvim-lspconfig",
-    config = function()
-        require("lazydev").setup({})
-        require("neoconf").setup({})
-        local lspconfig = require("lspconfig")
-        local capabilities = require("cmp_nvim_lsp").default_capabilities()
+	"neovim/nvim-lspconfig",
+	config = function()
+		require("lazydev").setup({})
+		require("neoconf").setup({})
+		local lspconfig = require("lspconfig")
+		local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-        for type, icon in pairs(diagnostic_signs) do
-            local hl = "DiagnosticSign" .. type
-            vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-        end
+		for type, icon in pairs(diagnostic_signs) do
+			local hl = "DiagnosticSign" .. type
+			vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+		end
 
-        -- lua
-        lspconfig.lua_ls.setup({
-            capabilities = capabilities,
-            on_attach = on_attach,
-            settings = {
-                Lua = {
-                    -- make the language server recognize "vim" global
-                    diagnostics = {
-                        globals = { "vim" },
-                    },
-                },
-            },
-        })
+		-- lua
+		lspconfig.lua_ls.setup({
+			capabilities = capabilities,
+			on_attach = on_attach,
+			settings = {
+				Lua = {
+					-- make the language server recognize "vim" global
+					diagnostics = {
+						globals = { "vim" },
+					},
+				},
+			},
+		})
 
-        -- json
-        lspconfig.jsonls.setup({
-            capabilities = capabilities,
-            on_attach = on_attach,
-            filetypes = { "json", "jsonc" },
-        })
+		-- json
+		lspconfig.jsonls.setup({
+			capabilities = capabilities,
+			on_attach = on_attach,
+			filetypes = { "json", "jsonc" },
+		})
 
-        -- python
-        lspconfig.pyright.setup({
-            capabilities = capabilities,
-            on_attach = on_attach,
-            settings = {
-                pyright = {
-                    disableOrganizeImports = false,
-                    analysis = {
-                        useLibraryCodeForTypes = true,
-                        autoSearchPaths = true,
-                        diagnosticMode = "workspace",
-                        autoImportCompletions = true,
-                    },
-                },
-            },
-        })
+		-- python
+		lspconfig.pyright.setup({
+			capabilities = capabilities,
+			on_attach = on_attach,
+			settings = {
+				pyright = {
+					disableOrganizeImports = false,
+					analysis = {
+						useLibraryCodeForTypes = true,
+						autoSearchPaths = true,
+						diagnosticMode = "workspace",
+						autoImportCompletions = true,
+					},
+				},
+			},
+		})
 
-        -- typescript
-        lspconfig.ts_ls.setup({
-            on_attach = on_attach,
-            capabilities = capabilities,
-            filetypes = {
-                "typescript",
-            },
-            root_dir = lspconfig.util.root_pattern("package.json", "tsconfig.json", ".git"),
-        })
+		-- typescript
+		lspconfig.ts_ls.setup({
+			on_attach = on_attach,
+			capabilities = capabilities,
+			filetypes = {
+				"typescript",
+			},
+			root_dir = lspconfig.util.root_pattern("package.json", "tsconfig.json", ".git"),
+		})
 
-        -- bash
-        lspconfig.bashls.setup({
-            capabilities = capabilities,
-            on_attach = on_attach,
-            filetypes = { "sh" },
-        })
+		-- bash
+		lspconfig.bashls.setup({
+			capabilities = capabilities,
+			on_attach = on_attach,
+			filetypes = { "sh" },
+		})
 
-        -- docker
-        lspconfig.dockerls.setup({
-            capabilities = capabilities,
-            on_attach = on_attach,
-        })
+		-- docker
+		lspconfig.dockerls.setup({
+			capabilities = capabilities,
+			on_attach = on_attach,
+		})
 
-        -- rust
-        lspconfig.rust_analyzer.setup({
-            settings = {
-                ["rust-analyzer"] = {},
-            },
-            capabilities = capabilities,
-            on_attach = on_attach,
-        })
+		-- rust
+		lspconfig.rust_analyzer.setup({
+			settings = {
+				["rust-analyzer"] = {},
+			},
+			capabilities = capabilities,
+			on_attach = on_attach,
+		})
 
-        -- markdown
-        lspconfig.marksman.setup({
-            capabilities = capabilities,
-        })
-    end,
-    lazy = false,
-    dependencies = {
-        "williamboman/mason.nvim",
-        "creativenull/efmls-configs-nvim",
-        "hrsh7th/nvim-cmp",
-        "hrsh7th/cmp-buffer",
-        "hrsh7th/cmp-nvim-lsp",
-    },
+		-- markdown
+		lspconfig.marksman.setup({
+			capabilities = capabilities,
+		})
+	end,
+	lazy = false,
+	dependencies = {
+		"williamboman/mason.nvim",
+		"hrsh7th/nvim-cmp",
+		"hrsh7th/cmp-buffer",
+		"hrsh7th/cmp-nvim-lsp",
+	},
 }
