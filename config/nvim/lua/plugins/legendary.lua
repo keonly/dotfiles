be8e930bda632df3683a8022033b3122a68aff96
keonly <70371393+keonly@users.noreply.github.com>
2024-10-10 01:53:47 +0900

    feat(nvim): cleanup plugins

--- config/nvim/lua/plugins/legendary.lua
+++
@@ -1,8 +0,0 @@
-return {
-    "mrjones2014/legendary.nvim",
-    priority = 10000,
-    lazy = false,
-    config = function()
-        require("legendary").setup({ extensions = { lazy_nvim = true } })
-    end,
-}
