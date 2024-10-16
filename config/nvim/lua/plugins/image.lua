da201ab9834a4ef6f1096259357c78cf62ad2657
keonly <70371393+keonly@users.noreply.github.com>
2024-10-16 19:20:16 +0900

    fix(nvim): extremely slow startup due to image.nvim

--- config/nvim/lua/plugins/image.lua
+++ config/nvim/lua/plugins/image.lua
@@ -1,13 +1,14 @@
 return {
     "3rd/image.nvim",
     lazy = true,
-    event = "VeryLazy",
+    ft = { "markdown" },
     config = function()
         package.path = package.path .. ";" .. vim.fn.expand("$HOME") .. "/.luarocks/share/lua/5.1/?/init.lua;"
         package.path = package.path .. ";" .. vim.fn.expand("$HOME") .. "/.luarocks/share/lua/5.1/?.lua;"
 
         require("image").setup({
             backend = "kitty",
+            tmux_show_only_in_active_window = true,
         })
     end,
 }
